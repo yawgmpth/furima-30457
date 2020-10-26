072fdeff2a61a2c9bc7eb50e4e66b176ff643a0d
yawgmpth <71745094+yawgmpth@users.noreply.github.com>
2020-10-26 22:50:36 +0900

    userモデルにバリデーション記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,21 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-end
+         
+              
+             
+           validates :nickname,         presence: true
+           validates :birthday,         presence: true
+                
+             with_options presence: true do
+            validates :first_name,:last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+            validates :first_name_kana,:last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
+            validates :password, length: { minimum: 6 } ,format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i}
+            
+                has_many :items
+                has_many :orders
+              end
+             
+        end
+
+    
