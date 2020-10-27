62b284dbc53be805f3fa1abce09db3559533ed82
yawgmpth <71745094+yawgmpth@users.noreply.github.com>
2020-10-27 14:30:43 +0900

    新規登録エラーメッセージ表示

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,18 +6,19 @@ class User < ApplicationRecord
          
               
              
-           validates :nickname,         presence: true
-           validates :birthday,         presence: true
-                
-             with_options presence: true do
-            validates :first_name,:last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-            validates :first_name_kana,:last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
-            validates :password, length: { minimum: 6 } ,format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i}
-            
-                has_many :items
-                has_many :orders
-              end
-             
-        end
+    has_many :items
+    has_many :orders
+    validates :nickname,         presence: true
+    validates :birthday,         presence: true
+        
+      with_options presence: true do
+    validates :first_name,:last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+    validates :first_name_kana,:last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
+    validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i}
+    
+    
+      end
+      
+end
 
     
