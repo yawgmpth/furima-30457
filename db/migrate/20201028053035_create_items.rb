3662c0aacd13d8851de95d09890681bab41fa682
yawgmpth <71745094+yawgmpth@users.noreply.github.com>
2020-10-28 15:26:38 +0900

    itemsテーブル生成

--- db/migrate/20201028053035_create_items.rb
+++ db/migrate/20201028053035_create_items.rb
@@ -1,15 +1,17 @@
 class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
-      t.string :nickname,           null: false
-      t.string :email,              null: false, default: ""
-      t.string :encrypted_password, null: false, default: ""
       
-      t.string :first_name,       null: false     
-      t.string :last_name,        null: false 
-      t.string :first_name_kana,  null: false  
-      t.string :last_name_kana,   null: false
-      t.date   :birthday,         null: false
+      t.string :name,                     null: false
+      t.text   :description,              null: false
+      t.integer :category_id ,            null: false
+      t.integer :condition_id,            null: false     
+      t.integer :delivery_cost_id,        null: false 
+      t.integer :delivery_area_id,        null: false  
+      t.integer :delivery_day_id,         null: false
+      t.integer :price ,                  null: false
+      t.references :user,                 foreign_key: true
+      
       t.timestamps
     end
   end
