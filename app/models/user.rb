5c38e96967c5553c632b7fc9714633d96187b696
yawgmpth <71745094+yawgmpth@users.noreply.github.com>
2020-10-28 09:35:23 +0900

    テストコード確認完了

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,13 +11,13 @@ class User < ApplicationRecord
     validates :nickname,         presence: true
     validates :birthday,         presence: true
         
-      with_options presence: true do
-    validates :first_name,:last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-    validates :first_name_kana,:last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
+    with_options presence: true do
+    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+    validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
+    validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
     validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i}
-    
-    
-      end
+    end
       
 end
 
