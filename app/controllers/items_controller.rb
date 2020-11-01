fa122f39d4a9cc15eb75936a971aa7585f98732d
yawgmpth <71745094+yawgmpth@users.noreply.github.com>
2020-11-01 16:21:53 +0900

    Rubocop修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,9 +2,8 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new]
 
   def index
-     @item = Item.all.order(created_at: :desc)
-     
-    end
+    @item = Item.all.order(created_at: :desc)
+  end
 
   def new
     @item = Item.new
