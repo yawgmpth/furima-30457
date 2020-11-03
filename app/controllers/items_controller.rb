7a043c3ba6be7a07c0e46d295b3ac3736541e745
yawgmpth <71745094+yawgmpth@users.noreply.github.com>
2020-11-03 12:51:17 +0900

    before_action再度まとめる

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new]
+  before_action :set_item, only: [:show, :edit, :update]
 
   def index
     @item = Item.all.order(created_at: :desc)
@@ -17,18 +18,20 @@ class ItemsController < ApplicationController
       render 'new'
     end
   end
- 
+
   def show
-    @item = Item.find(params[:id])
   end
-  
+
   def edit
-    @item = Item.find(params[:id])
   end
 
-
-
-
+  def update
+    if @item.update(items_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
 
   private
 
@@ -44,5 +47,9 @@ class ItemsController < ApplicationController
       :delivery_day_id,
       :price
     ).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
