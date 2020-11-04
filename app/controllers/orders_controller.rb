42d95bf627c9ac6e495e5c37712a33e9a515f8d8
yawgmpth <71745094+yawgmpth@users.noreply.github.com>
2020-11-04 21:50:40 +0900

    エラーメッセージ表示

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,15 +1,18 @@
 class OrdersController < ApplicationController
-
+  before_action :set_item, only: [:index, :create]
   def index
   @order_form = OrderForm.new
   end
 
   def create
     @order_form = OrderForm.new(orders_params)
-    @order_form .save
+    if @order_form.valid?
+        @order_form.save
     redirect_to root_path
+   else
+    render :index
   end 
-
+end
   private
 
   def orders_params
@@ -22,5 +25,10 @@ class OrdersController < ApplicationController
       :telephone,
       :order_id
       ).merge(user_id: current_user.id,item_id:params[:item_id])
+    end
+   
+    def set_item
+      @item = Item.find(params[:item_id])
+    end
+
   end
-end
--- app/models/order_form.rb
+++ app/models/order_form.rb
@@ -6,7 +6,6 @@ class OrderForm
     with_options presence: true do
     
     validates :post_code, format: { with: /\A\d{3}[-]\d{4}\z/ }
-  
     validates :delivery_area_id
     validates :city
     validates :address
