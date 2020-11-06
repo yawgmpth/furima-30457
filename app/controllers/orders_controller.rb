7543885cc340db738941e4b4c3588a686f3abdad
yawgmpth <71745094+yawgmpth@users.noreply.github.com>
2020-11-06 12:24:04 +0900

    クレジット機能実装、環境変数設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,34 +1,57 @@
 class OrdersController < ApplicationController
   before_action :set_item, only: [:index, :create]
+  before_action :authenticate_user!
+  before_action :set_order, only: [:index]
   def index
-  @order_form = OrderForm.new
+    @order_form = OrderForm.new
   end
 
   def create
     @order_form = OrderForm.new(orders_params)
     if @order_form.valid?
-        @order_form.save
-    redirect_to root_path
-   else
-    render :index
-  end 
-end
-  private
+      pay_item
+      @order_form.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
 
-  def orders_params
+  private
+  
+  def set_order
+  @order_form = OrderForm.new
+  if current_user.id == @item.user_id
+    redirect_to root_path
+  elsif @item.order
+    redirect_to root_path
+  end
+end
+
+    
+    
+    def orders_params
     params.require(:order_form).permit(
       :post_code,
-      :delivery_area_id, 
+      :delivery_area_id,
       :city,
       :address,
       :building,
       :telephone,
       :order_id
-      ).merge(user_id: current_user.id,item_id:params[:item_id])
-    end
-   
-    def set_item
-      @item = Item.find(params[:item_id])
-    end
-
+    ).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
   end
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def pay_item
+    Payjp.api_key = Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: orders_params[:token],
+      currency: 'jpy'
+    )
+  end
+end
--- app/models/order_form.rb
+++ app/models/order_form.rb
@@ -1,29 +1,28 @@
 class OrderForm
   include ActiveModel::Model
- 
-  attr_accessor :user_id, :item_id,:post_code , :delivery_area_id,:city ,:address ,:building,:telephone ,:order_id
-  
-    with_options presence: true do
-    
-    validates :post_code, format: { with: /\A\d{3}[-]\d{4}\z/ }
+
+  attr_accessor :user_id, :item_id, :post_code, :delivery_area_id, :city, :address, :building, :telephone, :order_id, :token
+
+  with_options presence: true do
+    validates :token
+    validates :post_code, format: { with: /\A\d{3}-\d{4}\z/ }
     validates :delivery_area_id
     validates :city
     validates :address
-    validates :building
-    validates :telephone, format: { with: /\A\d{11}\z/ } 
-    end
-  
-    def save
-        order = Order.create(user_id: user_id, item_id: item_id)
-      
-        Address.create( 
-        post_code: post_code, 
-        delivery_area_id: delivery_area_id,
-        city: city,
-        address: address,
-        building: building,
-        telephone: telephone,
-        order_id: order.id
-       )
-      end
-    end
+    validates :telephone, format: { with: /\A\d{11}\z/ }
+  end
+
+  def save
+    order = Order.create(user_id: user_id, item_id: item_id)
+
+    Address.create(
+      post_code: post_code,
+      delivery_area_id: delivery_area_id,
+      city: city,
+      address: address,
+      building: building,
+      telephone: telephone,
+      order_id: order.id
+    )
+  end
+end
