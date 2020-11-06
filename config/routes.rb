b1c36be151379d6c09f77066a72dfd1b4b112d50
yawgmpth <71745094+yawgmpth@users.noreply.github.com>
2020-11-06 14:17:21 +0900

    インデント修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
 
   devise_for :users
-  root to: "items#index"
-  resources :users
-      resources :items do
+    root to: "items#index"
+    resources :users
+    resources :items do
       resources :orders 
     end
   end  
