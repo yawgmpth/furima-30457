a407d63867ec29f905831d545c57e4b968766520
yawgmpth <71745094+yawgmpth@users.noreply.github.com>
2020-11-06 14:53:58 +0900

    差分コミット　修正

--- spec/models/order_form_spec.rb
+++ spec/models/order_form_spec.rb
@@ -10,10 +10,10 @@ describe OrderForm do
         expect(@order_form).to be_valid
       end
       it '建物名が空でも購入できること' do
-       @order_form.building = ''
+        @order_form.building = ''
         expect(@order_form).to be_valid
       end
-     end
+    end
 
     context '商品の購入がうまくいかないとき' do
       it 'tokenが空だと購入出来ない' do
@@ -31,7 +31,7 @@ describe OrderForm do
       it '郵便番号にハイフンなしでは購入出来ない' do
         @order_form.post_code = ''
         @order_form.valid?
-        expect(@order_form.errors.full_messages).to include("Post code is invalid")
+        expect(@order_form.errors.full_messages).to include('Post code is invalid')
       end
 
       it '都道府県のidが空の時購入出来ない' do
@@ -42,7 +42,7 @@ describe OrderForm do
       it '都道府県のidが１の時購入出来ない' do
         @order_form.delivery_area_id = 1
         @order_form.valid?
-        expect(@order_form.errors.full_messages).to include("Delivery area must be other than 1")
+        expect(@order_form.errors.full_messages).to include('Delivery area must be other than 1')
       end
       it '市区町村が空では購入出来ない' do
         @order_form.city = ''
@@ -62,12 +62,12 @@ describe OrderForm do
       it '電話番号が12桁以上では購入できない' do
         @order_form.telephone = 123_456_789_012
         @order_form.valid?
-        expect(@order_form.errors.full_messages).to include("Telephone is invalid")
+        expect(@order_form.errors.full_messages).to include('Telephone is invalid')
       end
       it '電話番号がハイフンなしでは購入できない' do
         @order_form.telephone = 299
         @order_form.valid?
-        expect(@order_form.errors.full_messages).to include("Telephone is invalid")
+        expect(@order_form.errors.full_messages).to include('Telephone is invalid')
       end
     end
   end
